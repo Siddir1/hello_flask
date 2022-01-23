5405bcabd628f92b3fd7b519bab33c115cc2658f
Siddir1 <97356987+Siddir1@users.noreply.github.com>
2022-01-24 00:34:51 +0530

    This content was read from git after commit 2nd time

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return "This content was read from git after commit"
+	return "This content was read from git after commit 2nd time"
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True)
